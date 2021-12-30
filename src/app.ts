b6374f46b739b29ac2637fcc5b3b5fd19319a1ca
Pooria Faramarzian <PooriaFaramarzian@gmail.com>
2021-12-30 22:08:26 +0330

    [feat] implement validation for adding new projects

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,59 @@
+// Validation
+interface Validatable {
+  value: string | number;
+  /** The input value is mandatory */
+  required?: boolean;
+  /** The min length that text input accepts */
+  minLength?: number;
+  /** The max length that text input accepts */
+  maxLength?: number;
+  /** The min number that number input accepts */
+  min?: number;
+  /** The max number that number input accepts */
+  max?: number;
+}
+
+function validate(validatableInput: Validatable) {
+  let isValid = true;
+
+  if (validatableInput.required) {
+    isValid = isValid && validatableInput.value.toString().trim().length !== 0;
+  }
+
+  if (
+    validatableInput.minLength != null &&
+    typeof validatableInput.value === 'string'
+  ) {
+    isValid =
+      isValid && validatableInput.value.length > validatableInput.minLength;
+  }
+
+  if (
+    validatableInput.maxLength != null &&
+    typeof validatableInput.value === 'string'
+  ) {
+    isValid =
+      isValid && validatableInput.value.length < validatableInput.maxLength;
+  }
+
+  if (
+    validatableInput.min != null &&
+    typeof validatableInput.value === 'number'
+  ) {
+    isValid = isValid && +validatableInput.value > validatableInput.min;
+  }
+
+  if (
+    validatableInput.max != null &&
+    typeof validatableInput.value === 'number'
+  ) {
+    isValid = isValid && validatableInput.value < validatableInput.max;
+  }
+
+  return isValid;
+}
+
+// Decorators
 function AutoBind(
   _: any,
   _2: string,
@@ -13,6 +69,7 @@ function AutoBind(
   };
 }
 
+// Project input class
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
@@ -45,14 +102,60 @@ class ProjectInput {
     this.peopleInputElement = <HTMLInputElement>(
       this.element.querySelector('#people')
     );
+
     this.configure();
     this.attach();
+  }
+
+  private gatherUserInput(): [string, string, number] | void {
+    const enteredTitle = this.titleInputElement.value;
+    const enteredDescription = this.descriptionInputElement.value;
+    const enteredPeople = this.peopleInputElement.value;
+
+    const validatableTitle: Validatable = {
+      value: enteredTitle,
+      required: true
+    };
+
+    const validatableDescription: Validatable = {
+      value: enteredDescription,
+      required: true
+    };
+
+    const validatablePrice: Validatable = {
+      value: enteredPeople,
+      required: true,
+      min: 1,
+      max: 5
+    };
+    if (
+      !validate(validatableTitle) ||
+      !validate(validatableDescription) ||
+      !validate(validatablePrice)
+    ) {
+      alert('Invalid Data, please try again');
+      return;
+    } else {
+      return [enteredTitle, enteredDescription, +enteredPeople];
+    }
+  }
+
+  private clearInputs() {
+    this.titleInputElement.value = '';
+    this.descriptionInputElement.value = '';
+    this.peopleInputElement.value = '';
   }
 
   @AutoBind
   private submitHandler(e: Event) {
     e.preventDefault();
-    console.log(this.titleInputElement.value);
+    const userInput = this.gatherUserInput();
+
+    if (Array.isArray(userInput)) {
+      const [title, desc, people] = userInput;
+      console.table({ title, desc, people });
+      this.clearInputs();
+    }
   }
 
   private configure() {
